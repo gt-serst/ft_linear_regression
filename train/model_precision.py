79b5f81f7bef7b89f1e46ff0b33479fc0b4b9e2c
Géraud t'Serstevens <geraudts@gmail.com>
2025-05-02 17:28:35 +0200

    mae calculation fixed

--- train/model_precision.py
+++ train/model_precision.py
@@ -3,8 +3,8 @@ def calculate_model_precision(cleaned_data, estimated_price):
 	mean_absolute_error = 0
 	# Calculate the error size between actual price and estimated price
 	for (price, (x, y)) in zip(estimated_price, cleaned_data):
-		error = price - y
+		error = abs(price - y)
 		mean_absolute_error += error
-	mean_absolute_error / len(cleaned_data)
+	mean_absolute_error = mean_absolute_error / len(cleaned_data)
 
 	return mean_absolute_error
